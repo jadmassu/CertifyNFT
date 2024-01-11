251c25759b6cc5f0ef048f543c47254bfcce23d6
eyerusalem <eyerusad12@gmail.com>
2024-01-11 03:47:32 +0300

    text position changed

--- backend/app.py
+++ backend/app.py
@@ -62,43 +62,54 @@ def add_info(image):
     font = cv2.FONT_HERSHEY_COMPLEX
     font_scale = 1
     font_color = (0, 0, 0)  # Black color
-    line_thickness = 2
-    underline_thickness = 1
+    line_thickness = 3
+    underline_thickness = 2
+    x_axis = 550
 
     req = urllib.request.urlopen(image)
     arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
     img = cv2.imdecode(arr, -1)
-    cv2.putText(img,"text", (800, 50), font,font_scale, font_color,5,cv2.LINE_AA)
-    cv2.putText(img,"Certificate", (800,130), font,font_scale, font_color,5,cv2.LINE_AA)
-    cv2.putText(img,"of Completion", (800, 200), font,font_scale, font_color,5,cv2.LINE_AA)
+    cv2.putText(img,"Oracle", (800, 160), font,2, font_color,3,cv2.LINE_AA)
+    cv2.putText(img,"Certificate ", (x_axis,300), font,5, font_color,3,cv2.LINE_AA)
+    cv2.putText(img,"of Completion", (650,400), font,3, font_color,3,cv2.LINE_AA)
+    
+  
     
  
 
     # Add recipient's name
-    name_position = (800, 200)  
-    cv2.putText(img, "Recipient's Name: " + recipient_name, name_position, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, font_color)
-    text_size, _ = cv2.getTextSize(recipient_name, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, line_thickness)
+    name_position = (650, 500)  
+    cv2.putText(img,  recipient_name, name_position, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, font_color)
+    text_size, _ = cv2.getTextSize(recipient_name, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, line_thickness)
     underline_y = name_position[1] + text_size[1] + underline_thickness
     underline_start = (name_position[0], underline_y)
     underline_end = (name_position[0] + text_size[0], underline_y)
-    cv2.line(image, underline_start, underline_end, font_color, underline_thickness)
+    cv2.line(img, underline_start, underline_end, font_color, underline_thickness)
     
-    text_position = (800, 200)
-    cv2.putText(img, "For completing online softer development course " , text_position, font, font_scale, font_color, line_thickness)
+    text_position = (x_axis, 600)
+    cv2.putText(img, "For completing online softer development course " , text_position, font, font_scale, font_color, 4)
 
     # Add training date
-    date_position = (800, 300)
-    cv2.putText(img, "Completion Date: " + training_date, date_position, font, font_scale, font_color, line_thickness)
+    date_position = (100, 900)
+    date_text_position = (100, 950)
+    
+    cv2.putText(img, training_date, date_position, font, font_scale, font_color, line_thickness,cv2.LINE_AA)
+    cv2.putText(img, "Completion Date", date_text_position, font, font_scale, font_color, line_thickness,cv2.LINE_AA)
+    
+    
+
 
    
 
     # Add trainer's name
-    trainer_position = (800, 500)
-    cv2.putText(img, "Trainer's Name: " + trainer_name, trainer_position, font, font_scale, font_color, line_thickness)
+    trainer_position = (1300, 900)
+    trainer_text_position = (1300, 950)
+    
+    cv2.putText(img,trainer_name, trainer_position, font, font_scale, font_color, line_thickness,cv2.LINE_AA)
+    cv2.putText(img, "Trainer", trainer_text_position, font, font_scale, font_color, line_thickness,cv2.LINE_AA)
+    
 
-    # Add additional details
-  
-    # Display the generated certificate image
+
 
     cv2.imshow('image', img)
     cv2.waitKey(0)
